4c47db3da40f10ff7ac48f4de548c9cd0daf631e
Thomas Farias <thomasfarias0995@gmail.com>
2024-01-22 21:14:17 -0300

    Estrutura de um Projeto Nextjs

---
+++ src/pages/cart.tsx
@@ -0,0 +1,22 @@
+// pages/products.tsx
+
+import { NextPage } from "next";
+import Head from "next/head";
+
+const Products: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Carrinho</title>
+        <meta name="description" content="Meu carrinho" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>
+        Carrinho
+      </h1>
+    </>
+  )
+}
+
+export default Products
